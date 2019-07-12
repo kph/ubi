3bd00a7767c460c2cd9c722aa4f0e2bc90cac14d
Kevin Paul Herbert <kph@platinasystems.com>
2019-07-12 06:01:45 -0700

    find.go: New FindVolume() API function
    Add a new API function to find a volume by name and return the
    associated Linux device.
    
    Signed-off-by: Kevin Paul Herbert <kph@platinasystems.com>

---
+++ find.go
@@ -0,0 +1,37 @@
+// Copyright © 2019 Platina Systems, Inc. All rights reserved.
+// Use of this source code is governed by the GPL-2 license described in the
+// LICENSE file.
+
+package ubi
+
+import (
+	"errors"
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
+
+var ErrVolumeNotFound = errors.New("Volume not found")
+
+// FindVolume locates the named volume on the given UBI device and
+// returns the associated Linux device.
+func FindVolume(ubiNum int32, volName string) (devName string, err error) {
+	prefix := "ubi" + strconv.Itoa(int(ubiNum))
+	baseDir := "/sys/devices/virtual/ubi/" + prefix + "/"
+
+	files, err := ioutil.ReadDir(baseDir)
+	for _, file := range files {
+		name := file.Name()
+		if !strings.HasPrefix(name, prefix+"_") {
+			continue
+		}
+		ubiName, err := ioutil.ReadFile(baseDir + name + "/name")
+		if err != nil {
+			return "", err
+		}
+		if strings.TrimSpace(string(ubiName)) == volName {
+			return "/dev/" + name, nil
+		}
+	}
+	return "", ErrVolumeNotFound
+}
