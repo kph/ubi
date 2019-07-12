ae7cbf36339904ff54e5625c35910589a509e9e6
Kevin Paul Herbert <kph@platinasystems.com>
2019-07-12 06:00:24 -0700

    open: improve godoc

--- open.go
+++ open.go
@@ -9,6 +9,8 @@ import (
 	"syscall"
 )
 
+// Open is used to open the UBI control device. The control device is
+// used to attach and detach UBI devices from their underlying MTD devices.
 func Open() (u Ubictrl, err error) {
 	u = Ubictrl{}
 	u.f, err = os.OpenFile("/dev/ubi_ctrl", syscall.O_RDWR, 0666)
